293e53e182dd15dfb1c75e5286059441a916af1c
Raul Tomaz <raultomazti@gmail.com>
2024-04-17 01:40:04 -0300

    finalizando a calculadora e seus componentes

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,50 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+import Calculadora from "./components/Calculadora.vue"
+
+  const estado = reactive({
+    num1: 0,
+    num2: 0,
+    resultado: 0
+  })
+
+  const mudaResultado = operador => {
+    const { num1, num2 } = estado
+
+    switch (operador) {
+      case "subtracao":
+        estado.resultado =  num1 - num2
+      case "multi":
+        estado.resultado =  num1 * num2
+      case "divisao":
+        if(num2!=0){
+          estado.resultado =  num1 / num2
+        } else {
+          alert("Não se pode dividir por 0")
+          estado.resultado = 0
+          return
+        }
+      case "soma":
+        estado.resultado =  num1 + num2
+        console.log(estado.resultado)
+      default:
+        return
+    }
+    
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="row text-center my-4">
+      <h1>Calculadora VueJS</h1>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="row mt-4 mb-4 align-content-center">
+      <Calculadora :define-num1="evento => estado.num1 = parseInt(evento.target.value)" :define-num2="evento => estado.num2 = parseInt(evento.target.value)" :muda-resultado="evento => mudaResultado(evento.target.value)" />
+    </div>
+    <div class="text-center fs-2">
+      Resultado: <br>
+      {{ estado.resultado }}
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,32 @@
+<script setup>
+    const props = defineProps(["defineNum1", "defineNum2", "mudaResultado"])
+</script>
+
+<template>
+    <div class="col">
+        <label>Numero 1:</label>
+        <input value="0" @change="$props.defineNum1" type="number">
+    </div>
+    <div class="col">
+        <label>Numero 2:</label>
+        <input value="0" @change="$props.defineNum2" type="number">
+    </div>
+    <div class="col-md-2">
+        <label>Operação:</label>
+        <select @change="$props.mudaResultado" class="form-control">
+            <option value="default">Escolha</option>
+            <option value="soma">Soma</option>
+            <option value="subtracao">Subtração</option>
+            <option value="multi">Multiplicação</option>
+            <option value="divisao">Divisão</option>
+        </select>
+    </div>
+</template>
+
+<style scoped>
+    input {
+        width: 100%;
+        height: 38px;
+        border-radius: 3px;
+    }
+</style>
